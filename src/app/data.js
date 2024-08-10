31ae9465c05cc58b241b662489ba6651e9932b64
runtyalien <omgadkar@gmail.com>
2024-08-11 05:06:45 +0530

    First Commit

---
+++ src/app/data.js
@@ -0,0 +1,57 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "ProCaptioner",
+    description: "Caption your Videos in a seconds",
+    date: "2022-08-15",
+    demoLink: "https://procaptioner.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Apple Vision Clone",
+    description: "Perfect clone of Apple Vision's website",
+    date: "2022-06-20",
+    demoLink: "https://apple-visionclone.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Video Call App",
+    description: "Video Call App made using WebRTC",
+    date: "2022-09-10",
+    demoLink: "https://videoakaomcall.netlify.app/lobby.html",
+  },
+  {
+    id: 3,
+    name: "Old Portfolio",
+    description: "My old portfolio website",
+    date: "2022-09-10",
+    demoLink: "https://omkargadkarportfolio.netlify.app/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/runtyalien",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/omkar-gadkar-55648a2a0/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        inter: ['var(--font-inter)']
+      },
+      colors: {
+        background: 'rgb(var(--background))',
+        foreground: 'rgb(var(--foreground))',
+        muted: 'rgb(var(--muted))',
+        accent: 'rgb(var(--accent))',
+      },
+      boxShadow: {
+        'glass-inset': 'inset 0 17px 5px -9px rgba(254,254,91,0.05)',
+        'glass-sm': '5px 5px 20px 0px rgba(254,254,91,0.3)'
+      },
+      animation:{
+        'spin-slow': 'spin 40s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+};
