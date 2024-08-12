dbd869172bc6bd40fe065a5dd82d5792419d7da3
runtyalien <omgadkar@gmail.com>
2024-08-13 02:42:31 +0530

    page title change

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -28,10 +28,10 @@ export default function Home() {
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
         <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">
-            OmKAr
+            OmkAr
           </h1>
           <p className="font-light text-foreground text-ls">
-          Meet the architect of these digital realms. I am OmKAr, a weaver of code and a creator of seamless experiences.
+          Meet the architect of these digital realms. I am OmkAr, a weaver of code and a creator of seamless experiences.
           </p>
         </div>
       </div>
